3e6a9643c9ef7b0ad645dce64ca35d6783558034
Stuart Berg <bergs@janelia.hhmi.org>
2018-12-09 13:04:28 -0500

    Fix launch script (include dockerhub account name)

--- launch.sh
+++ launch.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+## Create a container from the given image from the 'flyem' Dockerhub account.
+
+##
+## Usage: ./launch [image-name] [container-name]
+##
+## Examples:
+##
+##    ./launch # defaults to flyem-build
+##    ./launch flyem-build my-build-container
+##
+
 IMAGE_NAME=${1-flyem-build}
 CONTAINER_NAME=${2-${IMAGE_NAME}}
+
+ACCOUNT_NAME=flyem
 
 docker run -it \
     --name ${CONTAINER_NAME} \
@@ -9,5 +22,5 @@ docker run -it \
     -e HOST_USER_ID=$UID \
     -e HOST_GROUP_NAME="$(id -g -n $USERNAME || echo $USERNAME)" \
     -e HOST_GROUP_ID=$(id -g $USERNAME) \
-    ${IMAGE_NAME} \
+    ${ACCOUNT_NAME}/${IMAGE_NAME} \
 ##
